25d74e67a10c341dde82e876ad133b540b8ea384
Dorogono <76874036+Dorogono@users.noreply.github.com>
2021-06-07 19:36:23 +0900

    Add files via upload

---
+++ src/movieController.js
@@ -0,0 +1,88 @@
+/*
+You DONT have to import the Movie with your username.
+Because it's a default export we can nickname it whatever we want.
+So import Movie from "./models"; will work!
+You can do Movie.find() or whatever you need like normal!
+*/
+import Movie from "./models/Movie";
+
+// Add your magic here!
+export const home = async (req, res) => {
+    const movies = await Movie.find({}).sort({year: "desc"});
+    return res.render('home', {pageTitle: 'Home', movies});
+};
+
+export const getUpload = (req, res) => {
+    return res.render('upload', {pageTitle: 'Upload'});
+};
+
+export const postUpload = async (req, res) => {
+    const {title, summary, year, rating, genres} = req.body;
+    try{
+        await Movie.create({
+        title,
+        summary,
+        year,
+        rating,
+        genres: Movie.formatGenres(genres),
+        });
+        return res.redirect('/');
+    } catch(error){
+        return res.render('upload', {pageTitle: 'Upload', errorMessage: error._message,})
+    }
+};
+
+export const watch = async (req, res) => {
+    const {id} = req.params;
+    const movie = await Movie.findById(id);
+    if(!movie){
+        return res.render('404', {pageTitle: "Movie is not Exist."});
+    }
+    return res.render('watch', {pageTitle: movie.title , movie});
+};
+
+export const getEdit = async (req, res) => {
+    const {id} = req.params;
+    const movie = await Movie.findById(id);
+    if(!movie){
+        return res.render('404', {pageTitle: "Movie is not Exist."});
+    }
+    return res.render('edit', {pageTitle: `Edit Movie: ${movie.title}`, movie});
+};
+
+export const postEdit = async (req, res) => {
+    const {id} = req.params;
+    const {title, summary, year, rating, genres} = req.body;
+    const movie = await Movie.findById(id);
+    if (!movie) {
+        return res.render('404', {pageTitle: "Movie is not Exist."});
+    }
+    await Movie.findByIdAndUpdate(id, {
+        title,
+        summary,
+        year,
+        rating,
+        genres: Movie.formatGenres(genres),
+    });
+    return res.redirect(`/movies/${id}`);
+
+};
+
+export const remove = async (req, res) => {
+    const {id} = req.params;
+    await Movie.findByIdAndDelete(id)
+    return res.redirect('/');
+};
+
+export const search = async (req, res) => {
+    const {keyword} = req.query;
+    let movies = [];
+    if(keyword){
+        movies = await Movie.find({
+            title: {
+                $regex: new RegExp(keyword, "i"),
+            },
+        });
+    }
+    return res.render('search', {pageTitle: 'Search', movies});
+};
